b505bdce9f8426eca0a200de619828c4c705267e
Kosyanov Oleg <cookie.witn.jam@gmail.com>
2019-11-24 19:26:33 +0300

    change login to email in login and registration html

--- create_database.php
+++ create_database.php
@@ -1,8 +1,8 @@
 <?php
-	$servername = "";
-	$username = "";
-	$password = "";
-	$dbname = "";
+	$servername = "localhost";
+	$username = "id11700650_sleepy_admin";
+	$password = "123456";
+	$dbname = "id11700650_lecture";
 
 	$conn = new mysqli($servername, $username, $password, $dbname);
 	if ($conn->connect_error) 
@@ -10,6 +10,7 @@
 
     $sql="CREATE TABLE IF NOT EXISTS `accounts` (
   `id` int(6) unsigned NOT NULL,
+  `status` int(1)unsigned NOT NULL,
   `email` varchar(40) NOT NULL,
   `password` varchar(20) NOT NULL,
   `surname` varchar(20) NOT NULL,
@@ -17,7 +18,8 @@
   `university`  varchar(20),
   `department` varchar(7),
   `sity` varchar(20),
-  `country` varchar(20)
+  `country` varchar(20),
+  PRIMARY KEY (`id`)
 )DEFAULT charset=utf8;";
 
 
@@ -26,4 +28,12 @@ if ($conn->query($sql) === TRUE) {
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error. "<br>";
 }
+	$sql="INSERT INTO `accounts`(`id`, `status`, `email`, `password`, `surname`, `name`, `university`, `department`, `sity`, `country`) VALUES 
+  (0,'cookie.witn.jam@gmail.com','993d4cf31414bb99aaa2194ea970ba68',Kosyanov','Oleg');";
+
+ if ($conn->query($sql) === TRUE) {
+    echo "Insert value in table successfully". "<br>";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error. "<br>";
+} 
 ?>
