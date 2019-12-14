a14d769a3072433e4382745b0b7ffc3512ee9db6
sleepy.h <cookie.witn.jam@gmail.com>
2019-12-14 13:22:14 +0300

    add in index.php authorization version of site

--- index.php
+++ index.php
@@ -12,31 +12,47 @@ if (isset($_POST['username']) or isset($_SESSION['login'])) {
 		$result1 = $conn->query($query1);
 		$result2 = $conn->query($query2);
 		if($result1->num_rows) {
-			echo "res1";
-			print_r($result1->fetch_row());//set $session values and show user page
+			$res=$result1->fetch_row();//set $session values and show user page
+			$_SESSION['login']=$res[3];
+			$_SESSION['password']=$res[4];
+			$_SESSION['name']=$res[6];
+			$_SESSION['surname']=$res[5];
+			$_SESSION['university']=$res[7];
+			$_SESSION['country']=$res[10];
+			$_SESSION['departament']=$res[8];
+			$_SESSION['city']=$res[9];
+			show_page_log();
 		} else {
 			if ($result2->num_rows){
-				echo "res2";
-				print_r($result2->fetch_row());//set $session values and show user page
+				$res=$result2->fetch_row();//set $session values and show user page
+				$_SESSION['login']=$res[3];
+				$_SESSION['password']=$res[4];
+				$_SESSION['name']=$res[6];
+				$_SESSION['surname']=$res[5];
+				$_SESSION['university']=$res[7];
+				$_SESSION['country']=$res[10];
+				$_SESSION['departament']=$res[8];
+				$_SESSION['city']=$res[9];
+				show_page_log();
 			} else {
-				show_page('неправильная почта и пароль');	
+				show_page_nlog('неправильная почта и пароль');	
 			}
 		}
 	} else {
 		$query= "SELECT * FROM `accounts` WHERE login='".$_SESSION['login']."' and password='".$_SESSION['password']."';";
 		if (($conn->query($query))->num_rows){
-			echo "session authorizate";
+			show_page_log();
 			// show user page 
 		} else {
-			show_page('неправильная почта и пароль');
+			show_page_nlog('неправильная почта и пароль');
 			$_SESSION['login'] = '';
 			$_SESSION['password'] = '';
 		}
 	}
 } else {
-	show_page('');
+	show_page_nlog('');
 }
-function show_page($error){
+function show_page_nlog($error){
 	echo' 
 <!DOCTYPE html>
 <html>
@@ -49,7 +65,6 @@ function show_page($error){
 			<p><a href="/">Главная страница</a></p>
 			<p><a href="#">Список курсов</a></p>
 			<p><a href="#">Создать свой курс</a></p>
-			<p><a href="#">Выйти</a></p>
 		</div>
 		<div class="main">
 			<div class="right-menu">	
@@ -63,6 +78,66 @@ function show_page($error){
 						<p class="message">Не зарегистрированы? <br><a href="registration.php">Создайте пользователя!</a></p>
 					</form>
 				</div>
+			</div>
+		</div>
+		<div id="footer">
+			<p>МГТУ имени Н.Э.Баумана - ИУ4-13Б - Косьянов Олег Вячеславич</p>
+		</div>
+	</body>
+</html>';
+}
+function show_page_log(){
+	echo ' <!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="index.css">
+		<title>Главная страница</title>
+	</head>
+	<body>
+		<div class="header">
+			<p><a href="/">Главная страница</a></p>
+			<p><a href="#">Список курсов</a></p>
+			<p><a href="#">Создать свой курс</a></p>
+			<p><a href="#">Настройки</a></p>
+			<p><a href="#">Выйти</a></p>
+		</div>
+		<div class="main">
+			<div class="right-menu">
+				<div class="form">
+					<form class="account-form" style="font-family: sans-serif;">
+						<h3 style="color: black;" >'.$_SESSION['login'].'</h3>
+						<h5 style="color: gray;">'.$_SESSION['surname'].' '.$_SESSION['name'].'</h5>
+						<h5 style="color: gray;">'.$_SESSION['university'].' / '.$_SESSION['departament'].'</h5>
+						<h5 style="color: gray;">'.$_SESSION['country'].' / '.$_SESSION['city'].'</h5>
+						<P>----</P>
+						<P>----</P>
+						<P>----</P>
+						<P>----</P>
+						<h4>Top 5 your corses:</h4>
+						<ol style="color: green;">
+							<li>first</li>
+							<li>second</li>
+							<li>third</li>
+							<li>fourth</li>
+							<li>fifth</li>
+						</ol>
+						<h4>Top 5 corses:</h4>
+						<ol style="color: green;">
+							<li>first</li>
+							<li>second</li>
+							<li>third</li>
+							<li>fourth</li>
+							<li>fifth</li>
+						</ol>
+					</form>
+				</div>
+			</div>
+			<div class="main-menu">
+				<form class="intro">
+					<h2>Добро пожаловать на leсture-hall</h2>
+					<h4>Мы поможем вам познать новое или стать учителем других падаванов.Открывайте для себя новые горизонты познания, приобретая современые знания, позволяющие вам реализовать все свои мечты.</h4>
+					<h3>Вперед за мечтой, падаван.</h3>
+				</form>
 			</div>
 		</div>
 		<div id="footer">
